943ced008581265ab14e6daeaa480b336106a1e6
Carolina Jimenez Moreno <sistemasmi12@gmail.com>
2022-12-05 12:32:05 -0500

    Edit models path + Add home page + Edit view index

--- microservice.py
+++ microservice.py
@@ -20,21 +20,17 @@ app.config['JSON_AS_ASCII'] = False
 PORT = 3200
 HOST = "127.0.0.1"
 
-vectorizer = pickle.load(open('models/vectorizer.pickle', 'rb'))
-toxic_classifier = pickle.load(open('models/toxic_linear_classifier.model', 'rb'))
-severe_toxic_classifier = pickle.load(open('models/severe_toxic_classifier.model', 'rb'))
-obscene_classifier = pickle.load(open('models/obscene_classifier.model', 'rb'))
-threat_classifier = pickle.load(open('models/threat_classifier.model', 'rb'))
-insult_classifier = pickle.load(open('models/insult_classifier.model', 'rb'))
-identity_hate_classifier = pickle.load(open('models/identity_hate_classifier.model', 'rb'))
+vectorizer = pickle.load(open('models/vectorizer1.pickle', 'rb'))
+toxic_classifier = pickle.load(open('models/toxic_linear1_classifier.model', 'rb'))
+severe_toxic_classifier = pickle.load(open('models/severe_toxic_linear1_classifier.model', 'rb'))
+obscene_classifier = pickle.load(open('models/obscene_linear1_classifier.model', 'rb'))
+threat_classifier = pickle.load(open('models/threat_linear1_classifier.model', 'rb'))
+insult_classifier = pickle.load(open('models/insult_linear1_classifier.model', 'rb'))
+identity_hate_classifier = pickle.load(open('models/identity_hate_linear1_classifier.model', 'rb'))
 
-""" pickle.dump(vectorizer, open('models/vectorizer.pickle', 'wb'))
-pickle.dump(toxic_classifier, open('models/toxic_linear_classifier.model', 'wb'))
-pickle.dump(severe_toxic_classifier, open('models/severe_toxic_classifier.model', 'wb'))
-pickle.dump(obscene_classifier, open('models/obscene_classifier.model', 'wb'))
-pickle.dump(threat_classifier, open('models/threat_classifier.model', 'wb'))
-pickle.dump(insult_classifier, open('models/insult_classifier.model', 'wb'))
-pickle.dump(identity_hate_classifier, open('models/identity_hate_classifier.model', 'wb')) """
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 @app.route('/textbased_emotion', methods=['GET'])
 def predict_textbased_emotion():
@@ -42,7 +38,7 @@ def predict_textbased_emotion():
         text = request.args.get('text')
         if text:
             text_vector = vectorizer.fit_transform([text])
-            toxic_result = toxic_classifier.predict(text_vector).toarray()
+            toxic_result = toxic_classifier.predict(text_vector)
             severe_toxic_result = severe_toxic_classifier.predict(text_vector)
             obscene_result = obscene_classifier.predict(text_vector)
             threat_result = threat_classifier.predict(text_vector)
@@ -57,16 +53,21 @@ def predict_textbased_emotion():
             its_identity_hate = identity_hate_result[0] == 1
             inappropriate = its_toxic and its_severe_toxic and its_obscene and its_threat and its_insult and its_identity_hate
 
-            """ text_vector  = "text_vector"
-            toxic_result  = "toxic_result"
-            severe_toxic_result  = "severe_toxic_result"
-            obscene_result  = "obscene_result"
-            threat_result  = "threat_result"
-            insult_result  = "insult_result"
-            identity_hate_result  = "identity_hate_result"
-            inappropriate = True """
+            # toxic_result  = "toxic_result"
+            # severe_toxic_result  = "severe_toxic_result"
+            # obscene_result  = "obscene_result"
+            # threat_result  = "threat_result"
+            # insult_result  = "insult_result"
+            # identity_hate_result  = "identity_hate_result"
+            # inappropriate = True
 
-            emotion = "negative" if inappropriate else "positive"
+            emotion = "Negative" if inappropriate else "Positive"
+            
+            countries = ""
+            companies = ""
+            people = ""
+            dates = ""
+            hours = ""
 
             #return make_response(jsonify({'sentiment': result[0], 'text': text, 'status_code':200}), 200)
             return render_template(
@@ -80,8 +81,12 @@ def predict_textbased_emotion():
                     insult_result=insult_result,
                     identity_hate_result=identity_hate_result,
                     emotion=emotion,
+                    countries=countries,
+                    companies=companies,
+                    people=people,
+                    dates=dates,
+                    hours=hours,
                 )
-        #return make_response(jsonify({'error':'sorry! unable to parse', 'status_code':500}), 500)
         return render_template('error.html', error='Sorry! Unable to parse')
 
 if __name__ == "__main__":
