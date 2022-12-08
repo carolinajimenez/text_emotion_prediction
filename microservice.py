a8d6936903b3a6f4b667060d296f0f31388b4677
Carolina Jimenez Moreno <cjimenezm0794@gmail.com>
2022-12-08 15:45:23 -0500

    Update model + Add Readme + Edit script

--- microservice.py
+++ microservice.py
@@ -13,11 +13,15 @@ import json
 import pickle
 # Third party imports.
 import nltk
-from nltk import ne_chunk, pos_tag, word_tokenize
-from nltk.tree import Tree
+from nltk.stem import WordNetLemmatizer
 import pycountry
 from flask import Flask, render_template, make_response, jsonify, request
-from sklearn.feature_extraction.text import TfidfVectorizer
+import torch
+import torch.nn as nn
+
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# APP CONFIGURATIONS
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -25,18 +29,72 @@ app.config['JSON_AS_ASCII'] = False
 PORT = 3200
 HOST = "127.0.0.1"
 
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# INITIALIZE MODELS
+
 vectorizer = pickle.load(open(f'models/vectorizer2.pickle', 'rb'))
 emotion_classifier = pickle.load(open('models/emotion_classifier.model', 'rb'))
-# toxic_classifier = pickle.load(open('models/toxic_rbf_classifier.model', 'rb'))
-# severe_toxic_classifier = pickle.load(open('models/severe_toxic_rbf_classifier.model', 'rb'))
-# obscene_classifier = pickle.load(open('models/obscene_linear1_classifier.model', 'rb'))
-# threat_classifier = pickle.load(open('models/threat_linear1_classifier.model', 'rb'))
-# insult_classifier = pickle.load(open('models/insult_linear1_classifier.model', 'rb'))
-# identity_hate_classifier = pickle.load(open('models/identity_hate_linear1_classifier.model', 'rb'))
+
+with open('models/word_dict.json') as json_file:
+    word_dict = json.load(json_file)
+labels_wigths = 'models/model_26_87.12.pth'
+# 10-column embed model
+embedding = nn.Embedding(len(word_dict), 10)
 
 
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# FUNCTIONS
+
+# Base model: network of a neuron with 6 outputs
+class base_line(nn.Module):
+  def __init__(self,fin,out):
+    super(base_line,self).__init__()
+    self.out = out
+    self.fin = fin
+    self.fc1 = nn.Linear(self.fin,2048)
+    self.fc2 = nn.Linear(2048,1024)
+    self.fc3 = nn.Linear(1024,512)
+    self.relu = nn.ReLU()
+    self.fc4 = nn.Linear(512,self.out)
+    self.sigmoid = nn.Sigmoid()
+
+  def forward(self,x):
+    out = self.fc1(x)
+    out = self.fc2(out)
+    out = self.fc3(out)
+    our = self.relu(out)
+    out = self.fc4(out)
+    out = self.sigmoid(out)
+    return out
+
+model = base_line(10,6)
+model.load_state_dict(torch.load(labels_wigths, map_location=torch.device('cpu')))
+model.eval()
+
+def average_tensor(x):
+    """
+    Function that calculates average vector
+    """
+    tensor_d = torch.zeros((1,10))
+    for t in x:
+        tensor_d += t
+    return tensor_d/x.shape[0]
+
+def coded_words(x, word_dict):
+    return [word_dict[w] for w in x if w in word_dict]
+
+def converter(words):
+    return average_tensor( embedding(torch.tensor(coded_words(words.split(), word_dict))))
+
+nltk.download('omw-1.4')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
 def clean_text(text):
-    # Lemmatizing the texts
+    """
+    Process text - Lemmatizing the texts
+    """
     # removing aphostrophe words
     text = text.lower()
     text = re.sub(r"what's", "what is ",str(text)) 
@@ -53,16 +111,21 @@ def clean_text(text):
     text = re.sub(r"'scuse", " excuse ", str(text)) 
     text = re.sub('W', ' ', str(text)) 
     text = re.sub(' +', ' ', str(text))
+    # Remove hyperlinks
+    text = re.sub(r"https?://\S+|www\.\S+", ' ', str(text))
     # Remove punctuations, numbers and special characters
     text = re.sub('[^A-Za-z0-9]+', ' ', str(text))
+    text = lemmatizer.lemmatize(text)
     text = text.strip(' ')
     return text
 
 def get_dates(text):
-    # dd/mm/yyyy dd-mm-yyyy 
+    """
+    Get the dates that are in text
+    """
+    # dd/mm/yyyy; dd-mm-yyyy; dd mm yyyy 
     search = ""
-    vals = re.findall(r"[\d]{1,2}[/-][\d]{1,2}[/-][\d]{2,4}", text)
-    print(vals)
+    vals = re.findall(r"[\d]{1,2}[\s/-][\d]{1,2}[/-][\d]{2,4}", text)
     if vals:
         for i in vals:
             if search != "":
@@ -71,17 +134,36 @@ def get_dates(text):
         
     # dd MMM yyyy
     vals = re.findall(r"[\d]{1,2} [ADFJMNOS]\w* [\d]{2,4}", text)
+    if vals:
+        for i in vals:
+            if search != "":
+                search += ", "
+            search += f"{i}"
+    
+    # Mar; March
+    vals = re.findall(r'/^(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)$/i', text)
     print(vals)
     if vals:
         for i in vals:
             if search != "":
                 search += ", "
             search += f"{i}"
-    return search
+
+    # Mar-20-2009; Mar 20, 2009; March 20, 2009; Mar. 20, 2009; Mar 20 2009;
+    vals = re.findall(r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-zA-Z.,-]*[\s-]?(\d{1,2})?[,\s-]?[\s]?\d{4}', text, re.I|re.M)
+    if vals:
+        for i in vals:
+            if search != "":
+                search += ", "
+            search += f"{i}"
+
+    return "-" if search == "" else search
 
 def find_country(text):
+    """
+    Get the countries that are in text
+    """
     countries = ""
-    print(len(pycountry.countries))
     for country in pycountry.countries:
         if country.name in text:
             if countries != "":
@@ -90,6 +172,9 @@ def find_country(text):
     return "-" if countries == "" else countries
 
 def get_people_names(text):
+    """
+    Get the people names that are in text
+    """
     names = ""
     tokens = nltk.word_tokenize(text)
     tagged = nltk.pos_tag(tokens)
@@ -102,7 +187,8 @@ def get_people_names(text):
 
 
 
-
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# RUN APP
 
 @app.route('/')
 def home():
@@ -114,45 +200,41 @@ def predict_textbased_emotion():
         text = request.args.get('text')
         if text:
             text_cleaned = clean_text(text)
+            out = converter(text)
+            res = model(out)
+
+            toxic_result = "Yes" if res[0][0].item() > 0.29 else "No"
+            severe_toxic_result = "Yes" if res[0][1].item() > 0.29 else "No"
+            obscene_result = "Yes" if res[0][2].item() > 0.29 else "No"
+            threat_result = "Yes" if res[0][3].item() > 0.29 else "No"
+            insult_result = "Yes" if res[0][4].item() > 0.29 else "No"
+            identity_hate_result = "Yes" if res[0][5].item() > 0.29 else "No"
+
+            its_toxic = toxic_result == "Yes"
+            its_severe_toxic = severe_toxic_result == "Yes"
+            its_obscene = obscene_result == "Yes"
+            its_threat = threat_result == "Yes"
+            its_insult = insult_result == "Yes"
+            its_identity_hate = identity_hate_result == "Yes"
+            inappropriate = its_toxic and its_severe_toxic and its_obscene and its_threat and its_insult and its_identity_hate
+
             text_vector = vectorizer.fit_transform([text_cleaned])
-            # toxic_result = toxic_classifier.predict(text_vector)
-            # severe_toxic_result = severe_toxic_classifier.predict(text_vector)
-            # obscene_result = obscene_classifier.predict(text_vector)
-            # threat_result = threat_classifier.predict(text_vector)
-            # insult_result = insult_classifier.predict(text_vector)
-            # identity_hate_result = identity_hate_classifier.predict(text_vector)
-
-            # its_toxic = toxic_result[0] == 1
-            # its_severe_toxic = severe_toxic_result[0] == 1
-            # its_obscene = obscene_result[0] == 1
-            # its_threat = threat_result[0] == 1
-            # its_insult = insult_result[0] == 1
-            # its_identity_hate = identity_hate_result[0] == 1
-            # inappropriate = its_toxic and its_severe_toxic and its_obscene and its_threat and its_insult and its_identity_hate
-
-            toxic_result  = "toxic_result"
-            severe_toxic_result  = "severe_toxic_result"
-            obscene_result  = "obscene_result"
-            threat_result  = "threat_result"
-            insult_result  = "insult_result"
-            identity_hate_result  = "identity_hate_result"
-            inappropriate = True
-
             emotion = None
             try:
                 emotion = emotion_classifier.predict(text_vector)[0]
+                if emotion == "negative" and not inappropriate:
+                    emotion = "Positive"
             except Exception:
-                emotion = "negative" if inappropriate else "positive"
+                print("emotion: .-.")
+                emotion = "Negative" if inappropriate else "Positive"
 
-            companies = ""
             countries = find_country(text)
             paises = countries.split(', ')
             
+            #names = re.findall(r"[A-Z][a-z]+,?\s+(?:[A-Z][a-z]*\.?\s*)?[A-Z][a-z]+", text)
             names = get_people_names(text)
-            print(names)
             for p in paises:
                 names = names.replace(p, "")
-            print(names)
             
             dates = get_dates(text)
             fechas = dates.split(', ')
@@ -163,7 +245,7 @@ def predict_textbased_emotion():
 
             #people = re.findall(r"[A-Z][a-z]+,?\s+(?:[A-Z][a-z]*\.?\s*)?[A-Z][a-z]+", text)
 
-            hours = ""
+            hours = re.findall(r'([\d\s\w:]+) - ([\d\s\w:]+)', text)
 
             #return make_response(jsonify({'sentiment': result[0], 'text': text, 'status_code':200}), 200)
             return render_template(
@@ -178,7 +260,6 @@ def predict_textbased_emotion():
                     identity_hate_result=identity_hate_result,
                     emotion=emotion,
                     countries=countries,
-                    companies=companies,
                     people=people,
                     dates=dates,
                     hours=hours,
@@ -188,4 +269,4 @@ def predict_textbased_emotion():
 if __name__ == "__main__":
     print(f"Microserver running in port {PORT}")
     app.run(host=HOST, port=PORT)
-    print("\nGood bye!")
+    print("\n\nGood bye!")
